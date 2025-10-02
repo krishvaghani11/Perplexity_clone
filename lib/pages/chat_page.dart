c515af7ffd067308342948adc33051511057fd46
Your Name <krishvaghani453@gmail.com>
2025-10-02 11:23:19 +0530

    Initial commit

---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:perplexity_clone/theme/colors.dart';
+import 'package:perplexity_clone/widgets/answer_section.dart';
+import 'package:perplexity_clone/widgets/side_nav.dart';
+import 'package:perplexity_clone/widgets/sources_section.dart';
+
+class ChatPage extends StatelessWidget {
+  final String question;
+  const ChatPage({super.key, required this.question});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SideBar(),
+          const SizedBox(width: 100),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text(
+                  question,
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 24),
+                SourcesSection(),
+                AnswerSection(),
+                ],
+                ),
+              ),
+            ),
+          ),
+          Placeholder(
+            strokeWidth: 0,
+            color: AppColors.background,
+          )
+        ],
+      ),
+    );
+  }
+}
